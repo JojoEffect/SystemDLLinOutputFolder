58e91576efaf98aee608c0d39558a74b6ab80226
JojoEffect <33799886+JojoEffect@users.noreply.github.com>
2018-10-25 07:59:45 +0200

    Add project files.

---
+++ EntityFrameworkCoreLib/EntityFrameworkCoreClass.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EntityFrameworkCoreLib
+{
+    public class EntityFrameworkCoreClass
+    {
+    }
+}
---
+++ NetFramework472App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework472App
+{
+    using EntityFrameworkCoreLib;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EntityFrameworkCoreClass efcc = new EntityFrameworkCoreClass();
+            //NetStandard20Class nsc = new NetStandard20Class();
+        }
+    }
+}
